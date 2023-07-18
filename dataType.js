07f2ac008ba6bd97e63ada5252ac607efb12c3c5
prashantkumarpro <133010547+prashantkumarpro@users.noreply.github.com>
2023-07-18 11:35:52 +0000

    dataType.js

---
+++ dataType.js
@@ -0,0 +1,88 @@
+"use strict"// treat all JS code as newer version 
+
+
+// alert(2+2) // We are using node js not browser
+
+
+// console.log(2
+
+//     ) // code readabillity should be high
+
+// console.log("prashant")
+
+
+
+let age = 18;
+let isLoggedIn = false;
+let state = null;
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+let score = "33"
+console.log(typeof score); // string
+
+let scroe2 = 33;
+console.log(typeof scroe2); // number
+console.log(typeof (scroe2)); // number
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+console.log( typeof userName)
+
+console.log(typeof Boolean); // function
+console.log(typeof Symbol); // function
+
+
+
+
+
+
+
+
+
+
+
+// Data types in JS
+// Primitive , Reference(non-primitive)
+
+// primitive
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+
+
+
+
+
+
+
+
+const bigNum = 112343453243242343244n;
+// console.log(bigNum)//  112343453243242343244n;
+// console.log(typeof bigNum)// bigint
+
+// Reference (non-primitive)
+// Array, Objects, function
+
+const heroes = ['Saktiman', 'nagraj', 'doga'];
+
+let myObj = {
+    name: "prashant",
+    age: 23
+}
+
+// const myFunction = function () {
+//     return console.log("Hello World!")
+// }
+// myFunction();
+
+// console.log(typeof heroes);// Object
+// console.log(typeof myObj); // Object
+// console.log(typeof myFunction); // function 
